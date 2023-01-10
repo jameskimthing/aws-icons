de3c6b3944a38c84b52b8f171c6a861002347d04
James Kim <jameskimjaewon.abc@gmail.com>
2023-01-10 14:06:50 +0900

    Changed aws icons files (remove xml tag), and removed whitespace

---
+++ docs/_app/immutable/chunks/2-b2cad289.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-0cdd21c9.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,17 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+	kit: { adapter: adapter() }
+	// Gihub pages:
+	// kit: {
+	// 	adapter: adapter({
+    //         pages: 'docs',
+    //         assets: 'docs',
+    //     }),
+    //     paths: {
+    //         base: '/aws-icons'
+    //     }
+	// }
 };
 
 export default config;
