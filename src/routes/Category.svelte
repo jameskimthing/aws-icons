d4e666b6551383f0b9435334b3acf49b62acd5a6
jameskimthing <jameskimjaewon.abc@gmail.com>
2023-04-05 12:57:27 +0900

    small fix

--- src/routes/Category.svelte
+++ src/routes/Category.svelte
@@ -36,33 +36,31 @@
 </script>
 
 {#if showCategory}
-	<div class="mx-4 my-8">
-		{#if showCategoryTitle}
-			<div class="flex flex-row p-2 pr-8 mt-6 mb-2 border-b-2 border-white w-fit items-center">
-				{#if category !== 'general icons'}
-					<Icon {icon} />
-				{/if}
-				<div class="pl-4 text-3xl">{category.charAt(0).toUpperCase() + category.slice(1)}</div>
-			</div>
-		{/if}
-
-		{#if !$is_searching_categories}
-			<div bind:clientHeight={height}>
-				<div class="flex flex-row flex-wrap">
-					{#each Object.entries(architectureIcons) as [icon, content]}
-						<Architecture {icon} {category} />
-					{/each}
-				</div>
-
-				<div class="flex flex-row flex-wrap">
-					{#each Object.entries(resourceIcons) as [under, content]}
-						<Resource {under} {category} />
-					{/each}
-				</div>
-			</div>
-			{#if showCategoryTitle}
-				<div class="w-1/2 mx-auto border-b-2 my-12 border-gray-500" />
+	{#if showCategoryTitle}
+		<div class="flex flex-row p-2 pr-8 mt-6 mb-2 border-b-2 border-white w-fit items-center">
+			{#if category !== 'general icons'}
+				<Icon {icon} />
 			{/if}
+			<div class="pl-4 text-3xl">{category.charAt(0).toUpperCase() + category.slice(1)}</div>
+		</div>
+	{/if}
+
+	{#if !$is_searching_categories}
+		<div bind:clientHeight={height}>
+			<div class="flex flex-row flex-wrap">
+				{#each Object.entries(architectureIcons) as [icon, content]}
+					<Architecture {icon} {category} />
+				{/each}
+			</div>
+
+			<div class="flex flex-row flex-wrap">
+				{#each Object.entries(resourceIcons) as [under, content]}
+					<Resource {under} {category} />
+				{/each}
+			</div>
+		</div>
+		{#if showCategoryTitle}
+			<div class="w-1/2 mx-auto border-b-2 my-12 border-gray-500 mb-8" />
 		{/if}
-	</div>
+	{/if}
 {/if}
