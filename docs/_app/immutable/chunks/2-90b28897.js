97a15ea37d1532ec4585b055645a3e42ecb547ad
jameskimthing <jameskimjaewon.abc@gmail.com>
2023-04-14 16:21:50 +0900

    F i forgot to build

---
+++ docs/_app/immutable/chunks/2-90b28897.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-5c6abe4e.js";export{t as component};
--- docs/_app/immutable/chunks/2-9b2ff936.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b873b2e7.js";export{t as component};
